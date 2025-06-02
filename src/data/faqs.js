f9003a2ab00abbc58da8d872cbef9b608a8c45b4
Office97124 <Office@pdxrentready.com>
2025-06-02 16:33:29 -0700

    Delete src directory

--- src/data/faqs.js
+++
@@ -1,4 +0,0 @@
-export const faqs = [
-  { key: 'experience', q: 'How much experience do you have?', a: 'Over 10 years in the field.' },
-  { key: 'licensed', q: 'Are you licensed and insured?', a: 'Yes, fully licensed and insured in Oregon.' }
-];
